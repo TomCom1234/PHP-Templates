13e9ada05fbbf9e831e09a2b85ad40f6ac8e1006
Thomas Picolo-Donnelly <59615957+TomCom1234@users.noreply.github.com>
2022-05-04 09:37:07 +0100

    Reverted file

--- pdf_overlay_template.php
+++ pdf_overlay_template.php
@@ -67,6 +67,4 @@ $w->addPdf(__DIR__ . '/pdfs/PDF_FILE_NAME_HERE.pdf'); /* CHANGE THIS TO POINT TO
 //An Output
     $w->add('Hello, World', [100, 100, 10, 7]); /* html, [x, y, w, h] */
 
-//This is going to go into GitHub
-
-//This is going back to our local system
+//COMMENT HERE 
