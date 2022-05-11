a27fcb6526a1a9a0d218fb5bc45d877bff66cf98
Thomas Donnelly <tomcom12344@gmail.com>
2022-05-11 11:05:18 +0100

    Commited changes

--- pdf_overlay_template.php
+++ pdf_overlay_template.php
@@ -68,3 +68,5 @@ $w->addPdf(__DIR__ . '/pdfs/PDF_FILE_NAME_HERE.pdf'); /* CHANGE THIS TO POINT TO
     $w->add('Hello, World', [100, 100, 10, 7]); /* html, [x, y, w, h] */
 
 //COMMENT HERE 
+
+//NEw changes from Visual
